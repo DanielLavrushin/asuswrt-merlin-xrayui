aee059b450883b0419ab54bfe2fe39d954f9a398
Daniel Lavrushin <lavrushin@gmail.com>
2025-04-08 23:56:21 +0200

    refactor: simplify lock management by removing unused code and comments

--- src/backend/lock.sh
+++ src/backend/lock.sh
@@ -2,33 +2,12 @@
 # shellcheck disable=SC2034  # codacy:Unused variables
 
 check_lock() {
-    return
-    # if [ -f "$XRAYUI_LOCKFILE" ]; then
-    #     ageoflock=$(($(date +%s) - $(date +%s -r "$XRAYUI_LOCKFILE")))
-    #     if [ "$ageoflock" -gt 300 ]; then
-    #         printlog true "Lock file is older than 5 minutes. Removing stale lock." "$CERR"
-    #         kill "$(sed -n '1p' $XRAYUI_LOCKFILE)" >/dev/null 2>&1
-    #         clear_lock
-    #         echo "$$" >"$XRAYUI_LOCKFILE"
-    #         return 0
-    #     else
-    #         if [ -z "$1" ]; then
-    #             printlog true "Lock file is less than 5 minutes old. Exiting." "$CERR"
-    #             exit 1
-    #         else
-    #             printlog true "Another instance of $ADDON_TAG is running. Exiting." "$CERR"
-    #             return 1
-    #         fi
-    #     fi
-    # else
-    #     printlog true "No lock file found. Creating lock file." "$CSUC"
-    #     echo "$$" >"$XRAYUI_LOCKFILE"
-    #     return 0
-    # fi
+    LOCKFILE=/tmp/addonwebui.lock
+    FD=386
+    eval exec "$FD>$LOCKFILE"
+    flock -x "$FD"
 }
 
 clear_lock() {
-    return
-    # rm -f "$XRAYUI_LOCKFILE" 2>/dev/null
-    # return 0
+    flock -u "$FD"
 }

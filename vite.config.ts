fceab055313d64a653d37f2ba0be054615102f2f
xxhhlk <xxhhlk0@gmail.com>
2026-08-05 21:32:04 +0800

    fix: normalize CRLF line endings when inlining shell imports

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ function inlineShellImports(scriptPath, visited = new Set(), isRoot = true) {
   }
   visited.add(scriptPath);
 
-  let content = fs.readFileSync(scriptPath, 'utf8');
+  let content = fs.readFileSync(scriptPath, 'utf8').replace(/\r\n/g, '\n');
   const dirOfScript = dirname(scriptPath);
 
   const lines = content.split('\n');
